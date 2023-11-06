8599abfecf110af691efdfda8037726344b05ada
senghoho <qisstun@naver.com>
2023-11-07 00:05:33 +0900

    Feat: 날씨 모델 추가:fire:

--- nalssi/models.py
+++ nalssi/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# 날씨 모델
+class Weather(models.Model):
+    id = models.AutoField(primary_key=True)
+    # 외투
+    outer = models.CharField(max_length=50)
+    # 상의
+    top = models.CharField(max_length=50)
+    # 하의
+    pants = models.CharField(max_length=50)
